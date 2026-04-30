f419bbeee5de1fdc29fe6b9b6325a8ae28921fca
danielDominic <soulalex303@gmail.com>
2026-04-30 11:14:24 +0000

    removing bloated code

--- intro-webdev/script.js
+++ intro-webdev/script.js
@@ -4,16 +4,13 @@ button.addEventListener("click", () => {
 });
 
 const toggleButton = document.getElementById('theme-toggle');
-const body = document.body;
-
-body.classList.add('light-mode');
 
 toggleButton.addEventListener('click', () => {
-    if (body.classList.contains('light-mode')) {
-        body.classList.replace('light-mode', 'dark-mode');
-        toggleButton.textContent = 'Switch to Light Mode';
+    if(toggleButton.textContent === 'Dark Mode') {
+        document.body.classList.add('dark-mode');
+        toggleButton.textContent = 'Light Mode';
     } else {
-        body.classList.replace('dark-mode', 'light-mode');
-        toggleButton.textContent = 'Switch to Dark Mode';
+        document.body.classList.remove('dark-mode');
+        toggleButton.textContent = 'Dark Mode';
     }
 });
