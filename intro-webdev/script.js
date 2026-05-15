d25654c1d5bc2b5d5de40fe0ecbe9f7bd4958967
Reena <soulalex303@gmail.com>
2026-05-16 04:44:28 +0530

    added a functional input box with backend

--- intro-webdev/script.js
+++ intro-webdev/script.js
@@ -28,5 +28,10 @@ const dog = {
 const el = document.getElementById("code-block");
 el.innerText = JSON.stringify(dog, undefined, 6);
 
+const textBox = document.getElementById("textbox");
+
 const square = document.querySelector('.box-ex');
-square.style.backgroundColor = "crimson";
+
+textBox.addEventListener("change", function () {
+    square.style.backgroundColor = textBox.value;
+});
